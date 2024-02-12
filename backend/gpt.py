048d846be9f414b0864f438129747d4c5e5c163f
ym5528 <ym5528@rit.edu>
2024-02-11 22:09:26 -0500

    Was finally able to solve the issue of the generated query having a default limit of 5. Modified GUI to clear all inputs and outputs via a clear button.

--- backend/gpt.py
+++ backend/gpt.py
@@ -9,34 +9,19 @@ class Gpt:
         os.environ["OPENAI_API_KEY"] = ''
         self.db_path = "C:/Users/anark/Desktop/Capstone/Implementation/Modulated_code/backend/GeoQuery.db"
         self.db = SQLDatabase.from_uri(f"sqlite:///{self.db_path}")
-        self.llm = OpenAI(temperature = 0, max_tokens = 1000)
-        self.db_chain = SQLDatabaseChain.from_llm(self.llm, self.db, return_sql = True)
-        self.db_chain1 = SQLDatabaseChain.from_llm(self.llm, self.db, return_intermediate_steps = True)
+        self.llm = OpenAI(temperature = 0)
+        self.db_chain = SQLDatabaseChain.from_llm(self.llm, self.db, return_sql = True, top_k = 1)
+        self.db_chain1 = SQLDatabaseChain.from_llm(self.llm, self.db, return_intermediate_steps = True, top_k = 1)
         print("DB_chain successfull")
 
     def run_queries(self,query):
         self.response = self.db_chain(query)
-        #self.intermediateSteps=self.response["intermediate_steps"]
         self.answer=self.response["result"]
         self.response1 = self.db_chain1(query)
         self.answer1 = self.response1["result"]
-        #self.response1 = self.chain.invoke({"question": f"{query}"})
-        #print("query = ",self.response1)
-        #print("\n \n")
-        #print("self.response  = ", self.response)
-        #print("\n \n")
-        #print("self.intermediateSteps = ",self.intermediateSteps)
-        #print(self.answer)
-        #print("Response sql = ",self.response1["sql_query"])
         return [self.answer, self.answer1]
 
-        '''
-        self.response = self.chain.invoke({"question": f"{query}"})
-        print("query = ",self.response)
-        #print("solution = ",self.result)
-        print(self.db.run(self.response))
-        self.db_chain.run(query)
-        '''
+
 
         
         
--- frontend/app.py
+++ frontend/app.py
@@ -1,49 +1,3 @@
-'''
-import tkinter as tk
-from tkinter import messagebox
-#from main import process_query
-
-class App:
-
-    def __init__(self):
-        self.app = tk.Tk()
-        self.app.geometry("1500x750")
-        self.app.title("SQL Chatbot")
-        self.header = tk.Label(self.app, text="SQL Chatbot for GeoQuery Database", font = ("Helvetica", 40))
-        self.header.pack(pady = 10)
-        tk.Label(self.app, text="Enter the question you would like to ask:", font=("Helvetica", 20)).place(x = 40, y = 200)
-        self.textInput = tk.Text(self.app, height = 1, width = 90, font = ("Helvetica", 20), fg = "#0f63d1")
-        self.textInput.place(x=40, y=250)
-        send_button = tk.Button(self.app, text="Send", command = self.btn_send)
-        send_button.pack()
-        tk.Label(self.app, text="SQL Query:", font=("Helvetica", 20)).place(x = 40, y = 300)
-        self.textOutputQuery = tk.Text(self.app, height = 2, width = 90, font = ("Helvetica", 20), fg = "#34b816")
-        self.textOutputQuery.place(x=40, y=350)
-        self.textOutputQuery.config(state=tk.DISABLED)
-        tk.Label(self.app, text="Result:", font=("Helvetica", 20)).place(x = 40, y = 450)
-        self.textOutputAnswer = tk.Text(self.app, height = 1, width = 90, font = ("Helvetica", 20), fg = "#34b816")
-        self.textOutputAnswer.place(x=40, y=500)
-        self.textOutputAnswer.config(state=tk.DISABLED)        
-
-        self.app.mainloop()
-
-    def btn_send(self):
-        self.query = self.textInput.get("1.0", 'end-1c')
-        print(self.query + " Inside button")
-        #process_query(self.query)
-
-        
-    def query_out(self, answer):
-        self.textOutputQuery.insert(tk.END, answer)
-'''
-'''
-    def btn_send(self):
-        self.query = self.textInput.get("1.0",'end-1c')
-        print(self.query + "Inside button")
-        self.user_input = self.query
-        self.app.quit()
-        #return self.query
-'''
 
 import tkinter as tk
 from tkinter import messagebox
@@ -66,10 +20,12 @@ class App:
         self.textOutputQuery.place(x=40, y=350)
         self.textOutputQuery.config(state=tk.DISABLED)
         tk.Label(self.app, text="Result:", font=("Helvetica", 20)).place(x = 40, y = 450)
-        self.textOutputAnswer = tk.Text(self.app, height = 1, width = 90, font = ("Helvetica", 20), fg = "#34b816")
+        self.textOutputAnswer = tk.Text(self.app, height = 3, width = 90, font = ("Helvetica", 20), fg = "#34b816")
         self.textOutputAnswer.place(x=40, y=500)
         self.textOutputAnswer.config(state=tk.DISABLED)        
         self.callback = callback
+        self.clear_button = tk.Button(self.app, text="Clear", command = self.clear_textboxes)
+        self.clear_button.place(x=250, y=250)
         #self.textInput.bind("<return>", self.btn_send)
 
 
@@ -86,4 +42,13 @@ class App:
     def result_out(self, result):
         self.textOutputAnswer.config(state=tk.NORMAL)
         self.textOutputAnswer.insert(tk.END, result)
+        self.textOutputAnswer.config(state=tk.DISABLED)
+
+    def clear_textboxes(self):
+        self.textInput.delete(1.0, tk.END)
+        self.textOutputQuery.config(state=tk.NORMAL)
+        self.textOutputQuery.delete(1.0, tk.END)
+        self.textOutputQuery.config(state=tk.DISABLED)
+        self.textOutputAnswer.config(state=tk.NORMAL)
+        self.textOutputAnswer.delete(1.0, tk.END)
         self.textOutputAnswer.config(state=tk.DISABLED)
