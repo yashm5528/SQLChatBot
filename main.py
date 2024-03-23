b79f155c91cbe63178af23f64c65784b62087b43
yashm5528 <152750202+yashm5528@users.noreply.github.com>
2024-03-23 18:01:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from frontend import App
 def get_query(query):
     print(query)
     prompt = query
-    #try:
-    mygpt = Gpt()
-    request = mygpt.run_queries(prompt)
-    print(request[0], "\n", request[1], "\n", request[2])
-    ui.query_out(request[0])
-    ui.query_execute_out(request[2])
-    ui.result_out(request[1])
-    '''
+    try:
+        mygpt = Gpt()
+        request = mygpt.run_queries(prompt)
+        print(request[0], "\n", request[1], "\n", request[2])
+        ui.query_out(request[0])
+        ui.query_execute_out(request[2])
+        ui.result_out(request[1])
+    
     except:
         ui.query_out("An error has occured.")
         ui.result_out("Please contact the development team for assistance.")
-       ''' 
+     
 ui = App(get_query)
 ui.app.mainloop()
--- test.py
+++ test.py
@@ -1,41 +1,45 @@
 import sqlite3
 import pandas as pd
 from backend import Gpt
+import re
+import pickle
+
 mygpt = Gpt()
-#geography.uw.test.txt
-csv_data = pd.read_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt - Copy.csv")
+#geography.uw.test.txt - Copy
+csv_data = pd.read_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\book1.csv")
 records = csv_data.shape[0]
 
 df = pd.DataFrame()
 conn = sqlite3.connect(r'C:\Users\anark\Desktop\Capstone\SQLChatBot\backend\GeoQuery.db')
 cursor = conn.cursor()
-conn1 = sqlite3.connect(r'C:\Users\anark\Desktop\Capstone\SQLChatBot\backend\GeoQuery.db')
-cursor1 = conn1.cursor()
 for record in range(records):
     query = csv_data.iloc[record]['HumanQuery']
-    prompt = query + " " + "Reply like you are interacting with a human"
+    prompt = query
     golden_query = csv_data.iloc[record]['SQLQuery'] 
     try:
         result = mygpt.run_queries(prompt)
         golden_output = cursor.execute(golden_query)
         gold_result = golden_output.fetchall()
-        generated_sql_output = cursor1.execute(result[0])
-        generated_sql_result = generated_sql_output.fetchall()
-        new_record = {'HumanQuery': query, 'Gold Query': golden_query, 'Generated Query': result[0], 'Gold Result': gold_result,'Generated Result': generated_sql_result}#, 'Generated Answer': result[1]}
+        new_record = {'HumanQuery': query, 'Gold Query': golden_query, 'Generated Query': result[0], 'Gold Result': gold_result,'Generated Result': result[2], 'Generated Answer': result[1]}
         print(new_record)
         df = pd.concat([df, pd.DataFrame([new_record])], ignore_index=True)
     except:
         print("This question cannot be answered by this database")
         print("Please ask questions related to GeoQuery database")
+        new_record = {'HumanQuery': query, 'Gold Query': golden_query, 'Generated Query': "This question cannot be answered by this database", 'Gold Result': gold_result,'Generated Result': "[(NA,)]", 'Generated Answer': "NA"}
+        print(new_record)
+        df = pd.concat([df, pd.DataFrame([new_record])], ignore_index=True)
         record = record+1
 cursor.close()
 conn.close()
-cursor1.close()
-conn1.close()
 print(df)
 print('------------------------')
 print(df.iloc[:1])
 print('------------------------')
-df.to_excel(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt.results.xlsx")
-df.to_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt.results.csv")
+df.to_excel(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\book1.results.xlsx")
+df.to_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\book1.results.csv")
 
+with open('tuple_results.pkl', 'wb') as f:
+    pickle.dump(df, f)
+
+df.head(10)
