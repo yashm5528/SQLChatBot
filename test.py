5e14468364691033d398543c58319bc936ebff97
yashm5528 <152750202+yashm5528@users.noreply.github.com>
2024-04-20 15:33:09 -0400

    Changed paths to relative paths.
    Deleted dummy test files I used to check if the file contents were read and write correctly.

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ import pickle
 
 mygpt = Gpt()
 #geography.uw.test.txt - Copy
-csv_data = pd.read_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt.csv")
+csv_data = pd.read_csv(r"Testing\geography.uw.test.txt.csv")
 records = csv_data.shape[0]
 
 df = pd.DataFrame()
-conn = sqlite3.connect(r'C:\Users\anark\Desktop\Capstone\SQLChatBot\backend\GeoQuery.db')
+conn = sqlite3.connect(r'backend\GeoQuery.db')
 cursor = conn.cursor()
 for record in range(records):
     query = csv_data.iloc[record]['HumanQuery']
@@ -26,7 +26,9 @@ for record in range(records):
     except:
         print("This question cannot be answered by this database")
         print("Please ask questions related to GeoQuery database")
-        new_record = {'HumanQuery': query, 'Gold Query': golden_query, 'Generated Query': "This question cannot be answered by this database", 'Gold Result': gold_result,'Generated Result': "[(NA,)]", 'Generated Answer': "NA"}
+        new_record = {'HumanQuery': query, 'Gold Query': golden_query, 'Generated Query': 
+                      "This question cannot be answered by this database", 'Gold Result': gold_result,'Generated Result': 
+                      "[(NA,)]", 'Generated Answer': "NA"}
         print(new_record)
         df = pd.concat([df, pd.DataFrame([new_record])], ignore_index=True)
         record = record+1
@@ -36,8 +38,8 @@ print(df)
 print('------------------------')
 print(df.iloc[:1])
 print('------------------------')
-df.to_excel(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt.results.xlsx")
-df.to_csv(r"C:\Users\anark\Desktop\Capstone\SQLChatBot\Testing\geography.uw.test.txt.results.csv")
+df.to_excel(r"Testing\geography.uw.test.txt.results.xlsx")
+df.to_csv(r"Testing\geography.uw.test.txt.results.csv")
 
 with open('geography.uw.test.txt.results.pkl', 'wb') as f:
     pickle.dump(df, f)
